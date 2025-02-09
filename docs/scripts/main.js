de6bf48c10eb1673c153a4f9cea938e1f1377645
mansling <salman@mhs.politala.ac.id>
2025-02-09 12:15:20 +0800

    Update main.js

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -15,18 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
         .catch(error => console.error("Gagal memuat header:", error));
 
     // Mode Gelap
-      const toggle = document.getElementById("dark-mode-toggle");
-      if (toggle) {
-          toggle.addEventListener("click", function () {
-              document.body.classList.toggle("dark-mode");
-              localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
-          });
-  
-          // Pastikan dark mode tetap aktif saat halaman dimuat ulang
-          if (localStorage.getItem("darkMode") === "true") {
-              document.body.classList.add("dark-mode");
-          }
-      }
+    const toggle = document.getElementById("dark-mode-toggle");
+        if (toggle) {
+            toggle.addEventListener("click", function () {
+                document.body.classList.toggle("dark-mode");
+                localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
+            });
+    
+            // Pastikan dark mode tetap aktif saat reload
+            if (localStorage.getItem("darkMode") === "true") {
+                document.body.classList.add("dark-mode");
+            }
+        }
 
     // Fetch Data JSON
     fetch("data/comics.json")
